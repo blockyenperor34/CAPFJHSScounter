397ce5ebc11f40aaade083ee993cdc40539fab32
blockyenperor34 <50487680+blockyenperor34@users.noreply.github.com>
2022-04-22 22:45:31 +0800

    use roboto font

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        scaffoldBackgroundColor: const Color(0x000000),
+        //scaffoldBackgroundColor: const Color(0x000000),
+        fontFamily: 'Roboto',
+        scaffoldBackgroundColor: Colors.black,
         primarySwatch: Colors.blue,
       ),
       //home: const MyHomePage(title: 'Count'),
